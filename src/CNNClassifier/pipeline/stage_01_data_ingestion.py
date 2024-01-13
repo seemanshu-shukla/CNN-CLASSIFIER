a9b549fd892333fb2f1bd9ba107a32f60da28ca1
Seemanshu Shukla <seemanshu.shukla11@gmail.com>
2024-01-13 14:59:20 +0530

    DataIngestion pipeline to test/integrate component1

---
+++ src/CNNClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,15 @@
+from CNNClassifier.components.stage_01_data_ingestion import DataIngestion
+from CNNClassifier.config.configuration import ConfigurationManager
+from CNNClassifier import logger
+
+
+
+logger.info(f"data ingesiton stage started")
+
+config = ConfigurationManager()
+data_ingestion_config = config.get_data_ingestion_config()
+data_ingestion = DataIngestion(config=data_ingestion_config) #Here we feeded the input values needed by data ingestion component
+data_ingestion.download_file()
+data_ingestion.unzip_and_clean()
+
+logger.info(f"data ingesiton stage completed")
